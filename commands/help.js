d3995559099b6bf13c9083d1e78c17dc7b362f2d
Taehyeon Kim <1234tgk@gmail.com>
2021-07-08 14:07:31 -0400

    dynamic help command

--- commands/help.js
+++ commands/help.js
@@ -1,12 +1,21 @@
-const HELP_STRING = `
-!help: get help
-!randomreward <1 - 100>: try your luck to see if you can get a reward
-!coinflip <1 - 10000> flip many coins, see the result`
+const fs = require('fs')
+const dotenv = require('dotenv')
+dotenv.config()
+
+const commandFiles = fs
+  .readdirSync('./commands')
+  .filter((file) => file.endsWith('.js') && file !== 'help.js')
+
+let helpString = `\n  ${process.env.PREFIX}help: help new users`
+for (const file of commandFiles) {
+  const command = require(`./${file}`)
+  helpString += `\n  ${process.env.PREFIX}${command.name}: ${command.description}`
+}
 
 module.exports = {
   name: 'help',
   description: 'help new users',
   execute(message, args) {
-    message.reply(HELP_STRING)
+    message.reply(helpString)
   },
 }
